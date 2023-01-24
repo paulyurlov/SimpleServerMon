3a30f1420b716765e8cb21754d87bf95d112a38b
Paul Yurlov <palyurlov@gmail.com>
2023-01-24 20:35:39 +0300

    Minor update

--- app.py
+++ app.py
@@ -6,9 +6,19 @@ app = Flask(__name__)
 
 def get_data():
     tmp = {}
-    with open('/data/data.json') as f:
+    with open('data.json') as f:
         d = json.load(f)
-        tmp = d
+        # {"CPU": "1.6%", "MEM": "541Mi 7.7Gi", "CPU_TEMP": 37, "GPU_TEMP":         GPU Current Temp            : 44 C}
+        tmp['cpu_temp'] = float(d["CPU_TEMP"])
+        tmp['gpu_temp'] = float(
+            ''.join(list(filter(str.isdigit, d["GPU_TEMP"]))))
+        used, total = d["MEM"].split()
+        if "Mi" in used:
+            used = float(used.removesuffix('Mi')) / 1024
+        elif "Gi" in used:
+            used = float(used.removesuffix('Gi'))
+        tmp['mem_load'] = used / float(total.removesuffix('Gi')) * 100
+        tmp['cpu_load'] = float(d["CPU"].removesuffix('%'))
     return tmp
 
 
--- mon.sh
+++ mon.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-while true; do
-    python3 get_temps.py
+getData() {
+    CPUTEMP=$(sensors -j | jq '."coretemp-isa-0000" | ."Package id 0" | ."temp1_input"')
+    GPUTEMP=$(nvidia-smi -q -d temperature | awk 'FNR == 10 {print}')
+    MEM=$(free -h | awk '{print $3, $2}' | awk "NR == 2")
+    CPU=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
+    echo "{\"CPU\": \"$CPU\", \"MEM\": \"$MEM\", \"CPU_TEMP\": $CPUTEMP, \"GPU_TEMP\": \"$GPUTEMP\"}" >/home/paulyurlov/serv_mon/data.json
+}
+
+while :; do
+    getData
 done
